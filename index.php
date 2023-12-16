1e87f1cda08242cbab5e6529f39ff3563455f9c7
ujipn <hiroaki.uji8@gmail.com>
2023-12-16 13:07:47 +0900

    css修正

--- index.php
+++ index.php
@@ -11,11 +11,79 @@ include_once("./app/database/connect.php"); //データべ＾スと接続開始
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>掲示板</title>
+    <title>合宿メモ</title>
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
+<header class="header">
+        <div class="search-form">
+            <input type="text" id="location" placeholder="場所">
+            <input type="date" id="check-in-date" placeholder="チェックイン">
+            <input type="date" id="check-out-date" placeholder="チェックアウト">
+            <input type="number" id="guests" placeholder="人数" min="1">
+            <button id="search-button" type="submit">検索</button>
+        </div>
+    </header>
+<div id="myMap" style="width:60%;height:400px;margin:0 auto;"></div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        function GetMap() {
+            map = new Microsoft.Maps.Map('#myMap', {
+                center: new Microsoft.Maps.Location(35.68944, 139.69167),
+                zoom: 10
+            });
+
+            // Searchモジュールをロード
+            Microsoft.Maps.loadModule('Microsoft.Maps.Search', function() {
+                // SearchManagerを初期化
+                searchManager = new Microsoft.Maps.Search.SearchManager(map);
+            });
+        }
+
+        function updateMap(location) {
+            if (!searchManager) {
+                alert("検索マネージャーが初期化されていません。");
+                return;
+            }
+
+            // 検索リクエストを設定
+            const searchRequest = {
+                where: location,
+                callback: function(r) {
+                    if (r && r.results && r.results.length > 0) {
+                        const firstResult = r.results[0];
+                        const pin = new Microsoft.Maps.Pushpin(firstResult.location);
+                        map.entities.push(pin);
+                        map.setView({
+                            center: firstResult.location,
+                            zoom: 15
+                        });
+                    } else {
+                        alert('場所が見つかりませんでした。');
+                    }
+                },
+                errorCallback: function(e) {
+                    alert('エラーが発生しました。');
+                }
+            };
+
+            // 検索を実行
+            searchManager.geocode(searchRequest);
+        }
+
+        document.getElementById('search-button').addEventListener('click', function() {
+            const location = document.getElementById('location').value;
+            updateMap(location);
+        });
+
+        window.onload = GetMap;
+    </script>
+      <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script
+        src='https://www.bing.com/api/maps/mapcontrol?key=AoHPWA9yjgEBfopraPJU8a_aGvHCDmt_T8X5swu1glCtisNV2dM-HZ5Go52J44rq&callback=GetMap'
+        async defer></script>
+    <script src="js/BmapQuery.js"></script>
 
     <?php include("app/parts/header.php"); ?>
     <?php include("app/parts/validation.php"); ?>
